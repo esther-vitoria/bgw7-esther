61846af003c034983f35db5c4025eba7dd8d2efb
esther-vitoria <esther.vitoria21@outlook.com>
2025-07-14 19:29:09 -0300

    feat: done repository map and mock for tests

---
+++ goweb/desafio-encerramento-goweb/internal/repository/ticket_mock.go
@@ -0,0 +1,46 @@
+package repository
+
+import (
+	"app/desafio-goweb/internal"
+)
+
+// NewRepositoryTicketMock creates a new repository for tickets in a map
+func NewRepositoryTicketMock() *RepositoryTicketMock {
+	return &RepositoryTicketMock{}
+}
+
+// RepositoryTicketMock implements the repository interface for tickets
+type RepositoryTicketMock struct {
+	// FuncGet represents the mock for the Get function
+	FuncGet func() (t map[int]internal.TicketAttributes, err error)
+	// FuncGetTicketsByDestinationCountry
+	FuncGetTicketsByDestinationCountry func(country string) (t map[int]internal.TicketAttributes, err error)
+
+	// Spy verifies if the methods were called
+	Spy struct {
+		// Get represents the spy for the Get function
+		Get int
+		// GetTicketsByDestinationCountry represents the spy for the GetTicketsByDestinationCountry function
+		GetTicketsByDestinationCountry int
+	}
+}
+
+// GetAll returns all the tickets
+func (r *RepositoryTicketMock) Get() (t map[int]internal.TicketAttributes, err error) {
+	// spy
+	r.Spy.Get++
+
+	// mock
+	t, err = r.FuncGet()
+	return
+}
+
+// GetTicketsByDestinationCountry returns the tickets filtered by destination country
+func (r *RepositoryTicketMock) GetTicketsByDestinationCountry(country string) (t map[int]internal.TicketAttributes, err error) {
+	// spy
+	r.Spy.GetTicketsByDestinationCountry++
+
+	// mock
+	t, err = r.FuncGetTicketsByDestinationCountry(country)
+	return
+}
